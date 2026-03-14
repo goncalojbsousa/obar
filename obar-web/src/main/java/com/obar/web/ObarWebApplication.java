9de1903f117e3198db724a3dc94f2f1f2bf95fa6
goncalojbsousa <goncalojbsousa@gmail.com>
2026-03-14 18:39:08 +0000

    Init project

---
+++ obar-web/src/main/java/com/obar/web/ObarWebApplication.java
@@ -0,0 +1,13 @@
+package com.obar.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObarWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObarWebApplication.class, args);
+    }
+
+}

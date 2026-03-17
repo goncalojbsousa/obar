ef3f428ac109877e2e879bdff298ae2840c35679
Goncalo Sousa <goncalojbsousa@gmail.com>
2026-03-17 11:52:50 +0000

    feat: Implement initial JavaFX application structure and add Maven configuration for JavaFX

--- obar-desktop/src/main/java/com/obar/App.java
+++ obar-desktop/src/main/java/com/obar/App.java
@@ -1,13 +1,29 @@
 package com.obar;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import atlantafx.base.theme.PrimerLight;
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Application.setUserAgentStylesheet(new PrimerLight().getUserAgentStylesheet());
+
+        Label title = new Label("OBAR Desktop Teste");
+        title.setStyle("-fx-font-size: 28px; -fx-font-weight: bold;");
+
+        StackPane root = new StackPane(title);
+        root.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(root, 700, 420);
+        stage.setTitle("OBAR TESTE");
+        stage.setScene(scene);
+        stage.show();
     }
+
 }

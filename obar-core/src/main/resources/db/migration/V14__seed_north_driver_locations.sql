b0613e7c8d29cd45a0275fd42d6409022f6143d8
Goncalo Sousa <goncalojbsousa@gmail.com>
2026-06-01 15:17:57 +0100

    Seed driver locations for dispatch testing

---
+++ obar-core/src/main/resources/db/migration/V14__seed_north_driver_locations.sql
@@ -0,0 +1,15 @@
+UPDATE users
+SET
+    current_latitude = 41.6931991577,
+    current_longitude = -8.8328695297,
+    last_location_update = now ()
+WHERE
+    email = 'carlos.driver@gmail.com';
+
+UPDATE users
+SET
+    current_latitude = 41.5454486,
+    current_longitude = -8.426507,
+    last_location_update = now ()
+WHERE
+    email = 'ricardo.driver@gmail.com';

9513dde019c03e20f2f7c1af7e51e4066ff95528
goncalojbsousa <goncalojbsousa@gmail.com>
2026-06-13 16:17:27 +0100

    fix: Finalize trip VAT defaults

--- obar-core/src/main/resources/db/migration/V18__add_trip_tax_rate.sql
+++ obar-core/src/main/resources/db/migration/V18__add_trip_tax_rate.sql
@@ -2,11 +2,19 @@ ALTER TABLE trips
     ADD COLUMN tax_rate_applied DECIMAL(5, 4);
 
 UPDATE
-    trips
+    trips t
 SET
-    tax_rate_applied = 0.2300
+    tax_rate_applied = CASE WHEN TRIM(u.tax_number)
+    LIKE '6%' THEN
+        0.0000
+    ELSE
+        0.2300
+    END
+FROM
+    users u
 WHERE
-    tax_rate_applied IS NULL;
+    t.client_id = u.id
+    AND t.tax_rate_applied IS NULL;
 
 ALTER TABLE trips
     ALTER COLUMN tax_rate_applied SET NOT NULL;
